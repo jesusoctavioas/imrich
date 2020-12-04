3c59c75b77d8ee8114d2a6d181765a81e899f533
Jesus octavio acosta santos <jesusoctavioas@gmail.com>
2020-12-03 19:58:01 -0700

    commit inicial

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Jesus Octavio Acosta Santos on 29/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
